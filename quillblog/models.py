537dd20511e31f85510d10ebf5fa440121484716
Samin <samincgs@gmail.com>
2025-03-26 21:35:44 -0400

    resuming quill blog

--- quillblog/models.py
+++ quillblog/models.py
@@ -19,6 +19,7 @@ class User(db.Model, UserMixin): # UserMixin is a class inherited from flaskLogi
         return f'User("{self.username}, {self.email}, {self.image_file}")'
     
     
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
